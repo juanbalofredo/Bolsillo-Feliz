c5811dbb1077c961e63fa9de2eb6cdc7fa3d0f2d
Juan Bautista Lofredo <juanlofredocs6sj@gmail.com>
2023-03-14 23:38:04 -0300

    arreglos css

--- client/src/components/order/Orden.jsx
+++ client/src/components/order/Orden.jsx
@@ -1,33 +1,41 @@
-import React from "react"
-import "./Orden.css"
+import React from "react";
+import "./Orden.css";
 import { useDispatch, useSelector } from "react-redux";
 import { getCategoryParams } from "../../redux/apiPetitions/productsPetitions";
 import SearchBar from "../Navbar/SearchBar";
-import {  resPage } from "../../redux/slice/globalSlice";
-
+import { resPage } from "../../redux/slice/globalSlice";
 
 const Orden = () => {
   const dispatch = useDispatch();
-  const { brand, category,pname } = useSelector((state) => state.bolsilloFeliz);
+  const { brand, category, pname } = useSelector(
+    (state) => state.bolsilloFeliz
+  );
 
   async function changeOrder(e) {
-    dispatch(resPage())
-    getCategoryParams(dispatch, category, brand, e.target.value,pname)
+    dispatch(resPage());
+    getCategoryParams(dispatch, category, brand, e.target.value, pname);
   }
 
-  return (<>
-    <div className="orden-tecto-1"><h2>Elija su producto para comparar</h2>
-      <h3>{category === "all" ? "Todos los productos" : category}</h3>
-    </div>
-    <div className='orde-pag'>
-      <label value="" >Ordenar Por</label>
-      <select onChange={changeOrder} name="ordenPrecio" id="ordenPrecio" key={category}>
-        <option value="ASC">A-Z</option>
-        <option value="DESC">Z-A</option>
-      </select>
-      <SearchBar />
-    </div>
-  </>)
-}
+  return (
+    <>
+      <div className="orde-pag">
+        <div className="ord-txt-a">
+        <label value="">Ordenar Por</label>
+        <select
+          onChange={changeOrder}
+          name="ordenPrecio"
+          id="ordenPrecio"
+          key={category}
+        >
+          <option value="ASC">A-Z</option>
+          <option value="DESC">Z-A</option>
+        </select></div>
+        <div className="orden-tecto-1">
+          <h3>{category === "all" ? "Todos los productos" : category}</h3>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Orden
+export default Orden;
