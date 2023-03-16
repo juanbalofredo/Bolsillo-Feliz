fe567186b1634f868d95142cdad9ed17c0362e79
Juan Bautista Lofredo <juanlofredocs6sj@gmail.com>
2023-03-16 11:25:28 -0300

    mapita

--- client/src/views/home/Home.jsx
+++ client/src/views/home/Home.jsx
@@ -25,7 +25,13 @@ const Home = () => {
       getProductos(dispatch);
     }
   }, [dispatch]);
-  
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      console.log("Latitude is :", position.coords.latitude);
+      console.log("Longitude is :", position.coords.longitude);
+    });
+  });
 
   return (
     <>
