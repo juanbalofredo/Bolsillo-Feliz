7ca4aac6831418b76ad55f5ce3bc324bb10a272b
NicoGalarce1 <nicolasgalarce18@gmail.com>
2023-03-17 18:17:56 +0100

    Arreglando Cosas

--- api/src/helpers/createUser.helper.js
+++ api/src/helpers/createUser.helper.js
@@ -25,6 +25,7 @@ export default async function createUser(userFromBody) {
         })
     } else {
         creatingUser = await Users.create(userFromBody)
+        return creatingUser;
     }
     return creatingUser ;
 }
