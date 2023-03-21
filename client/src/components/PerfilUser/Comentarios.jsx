bb93c2300761d7d51a8ee18b8a64f00c41cee07e
Juan Bautista Lofredo <juanlofredocs6sj@gmail.com>
2023-03-21 18:51:36 -0300

    comentatrios perfil

--- client/src/components/PerfilUser/Comentarios.jsx
+++ client/src/components/PerfilUser/Comentarios.jsx
@@ -6,6 +6,8 @@ import "./Perfil.css";
 
 
 export default function Comentarios() {
+  const estate = useSelector((state) => state.bolsilloPersist);
+  const state = useSelector((state) => state.bolsilloFeliz);
   const [Comentarios, setComentarios] = useState("");
   const {id} = useSelector((state)=> state.bolsilloPersist);
   useEffect(() => {
@@ -16,13 +18,29 @@ export default function Comentarios() {
         return err;
       });
   }, [id]);
+  console.log(Comentarios)
   return (
     <>
     <div className="container_comentarios">
       {Comentarios ?( 
-        <div className="c">
-          Aca van los comentarios
+        Comentarios.map((a) => (
+          <div className="comentario-superm">
+          <img src={a.user.avatar} alt="img" />
+          <div className="esestetx">
+            <div className="txtaaassa">
+              <h4>
+                {a.user.name} {a.user.last_name}
+              </h4>
+              <div className="label-2-su">
+                <p>{"★".repeat(a.score)}</p>{"★".repeat(5-a.score)}
+              </div>
+            </div>
+            <div>
+              <label className="label-1-su">{a.message}</label>
+            </div>
+          </div>
         </div>
+        ))
      ):
       (
         <div>
