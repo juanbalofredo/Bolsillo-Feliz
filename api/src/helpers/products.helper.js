639dac8c6562c59158034fd646234b3dd48c01ec
pacodinucci <franciscoldinucci@gmail.com>
2023-03-23 10:53:33 -0300

    products que solo tengan precio, ahora si

--- api/src/helpers/products.helper.js
+++ api/src/helpers/products.helper.js
@@ -13,6 +13,7 @@ export async function getAllProducts() {
                 {
                     model: Prices,
                     attributes: ['price'],
+                    where: { price: {[Op.ne]: null} },
                     include: [
                         {
                             model: SuperM,
@@ -21,9 +22,6 @@ export async function getAllProducts() {
                     ]
                 }
             ],
-            where: {
-                '$Prices.id$': {[Op.ne]: null }
-            },
             order: [["name", "ASC"]]
         }
     )
