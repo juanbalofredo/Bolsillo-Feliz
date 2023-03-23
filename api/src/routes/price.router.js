55481dd16dc790216cec5128d837896645ea6770
Terry <danyel.martel@gmail.com>
2023-03-22 21:27:20 -0500

    agregado rutas price

--- api/src/routes/price.router.js
+++ api/src/routes/price.router.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { postEspecialPrice, postNonEspecialtPrice } from "../controllers/controller.price.js";
+import { postEspecialPrice, postNonEspecialtPrice, putPrice } from "../controllers/controller.price.js";
 
 const price = Router();
 
 price.post("/nonespecial", postNonEspecialtPrice)
+price.put("/", putPrice)
 // price.post("/especial", postEspecialPrice)
 
 
